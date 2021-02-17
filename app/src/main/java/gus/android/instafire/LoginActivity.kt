8fdac9ec707a9dfeaed3c3c426f77638ad0b3fef
Gus-2 <demeeus>
2021-02-17 09:33:08 +0100

    Adding Firebase Authentication

--- app/src/main/java/gus/android/instafire/LoginActivity.kt
+++ app/src/main/java/gus/android/instafire/LoginActivity.kt
@@ -2,7 +2,9 @@ package gus.android.instafire
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
 private const val TAG = "LoginActivity"
@@ -20,6 +22,25 @@ class LoginActivity : AppCompatActivity() {
                 Toast.makeText(this, "Email/Password cannot be empty !", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
+
+            // Firebase authentication check
+            val auth = FirebaseAuth.getInstance()
+            // These methods are asynchronous so we need to listen to them
+            // to be notified when the task is done
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    if(task.isSuccessful) {
+                        Toast.makeText(this, "Success !", Toast.LENGTH_SHORT).show()
+                        goPostActivity()
+                    } else {
+                        Log.e(TAG, "signInWithEmail", task.exception)
+                        Toast.makeText(this, "Authentication failed !", Toast.LENGTH_SHORT).show()
+                    }
+                }
         }
+    }
+
+    private fun goPostActivity() {
+        Log.i(TAG, "goPostActivity")
     }
 }
