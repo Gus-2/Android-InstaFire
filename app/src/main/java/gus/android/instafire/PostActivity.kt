08b3bef92031e930099ad6acc14f22920cb9eae6
Gus-2 <demeeus>
2021-02-18 10:28:54 +0100

    Refactor ProfileActivity to heritate from PostActivity.

--- app/src/main/java/gus/android/instafire/PostActivity.kt
+++ app/src/main/java/gus/android/instafire/PostActivity.kt
@@ -7,15 +7,20 @@ import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
 import gus.android.models.Post
+import gus.android.models.User
 import kotlinx.android.synthetic.main.activity_post.*
+import kotlin.math.sign
 
 private const val TAG = "PostActivity"
+private const val EXTRA_USERNAME = "EXTRA_USERNAME"
 
-class PostActivity : AppCompatActivity() {
+open class PostActivity : AppCompatActivity() {
 
+    private var signedInUser: User? = null
     private lateinit var fireStoreDb: FirebaseFirestore
     private lateinit var posts: MutableList<Post>
     private lateinit var adapter: PostsAdapter
@@ -35,7 +40,25 @@ class PostActivity : AppCompatActivity() {
 
         fireStoreDb = FirebaseFirestore.getInstance()
 
-        val postsReference = fireStoreDb.collection("posts").limit(20).orderBy("creation_time_ms", Query.Direction.DESCENDING)
+        fireStoreDb.collection("users")
+            .document(FirebaseAuth.getInstance().currentUser?.uid as String)
+            .get()
+            .addOnSuccessListener { userSnapshot ->
+                signedInUser = userSnapshot.toObject(User::class.java)
+                Log.i(TAG, "signed in user: ${signedInUser}")
+            }
+            .addOnFailureListener{
+                Log.i(TAG, "Failure fetching signed in user", it)
+            }
+
+        var postsReference = fireStoreDb.collection("posts").limit(20)
+            .orderBy("creation_time_ms", Query.Direction.DESCENDING)
+
+        val username = intent.getStringExtra(EXTRA_USERNAME)
+        if (username != null) {
+            supportActionBar?.title = username
+            postsReference = postsReference.whereEqualTo("user.username", username)
+        }
 
         postsReference.addSnapshotListener { snapshot, exception ->
             if (exception != null || snapshot == null) {
@@ -58,6 +81,7 @@ class PostActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (item.itemId == R.id.menu_profile) {
             val intent = Intent(this, ProfileActivity::class.java)
+            intent.putExtra(EXTRA_USERNAME, signedInUser?.username)
             startActivity(intent)
         }
 
